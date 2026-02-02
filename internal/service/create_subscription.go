7887fbe2032a8b28267cd2394a6ee31a02519308
programmer8760 <programmer8760@gmail.com>
2026-02-02 22:28:32 +0300

    chore: add CreateSubscription method for SubscriptionService

---
+++ internal/service/create_subscription.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"context"
+	"strings"
+
+	"github.com/prajkin/em-test-task/internal/domain"
+)
+
+func (s *SubscriptionsService) CreateSubscription(ctx context.Context, sub domain.Subscription) (domain.Subscription, error) {
+	if sub.Name = strings.TrimSpace(sub.Name); sub.Name == "" {
+		return domain.Subscription{}, domain.ErrInvalidName
+	}
+	if sub.Price <= 0 {
+		return domain.Subscription{}, domain.ErrInvalidPrice
+	}
+	if sub.StartDate.IsZero() {
+		return domain.Subscription{}, domain.ErrInvalidStartDate
+	}
+
+	if err := s.repo.Create(ctx, &sub); err != nil {
+		return domain.Subscription{}, err
+	}
+
+	return sub, nil
+}
