67f3b33585a91bdf21f295ea6d72dadb0104d43f
programmer8760 <programmer8760@gmail.com>
2026-02-02 22:28:32 +0300

    chore: add connection to db

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/prajkin/em-test-task
 
 go 1.25.3
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.11.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/prajkin/em-test-task/database"
+)
+
+func main() {
+	godotenv.Load()
+
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("db connected succesfully")
+}
