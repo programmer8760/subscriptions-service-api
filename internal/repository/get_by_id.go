52be89e8e3d18e82ede9f1b813224446bbac0a57
programmer8760 <programmer8760@gmail.com>
2026-02-03 03:24:43 +0300

    fix: use sql.NullTime when fetching sub's end date

--- internal/repository/get_by_id.go
+++ internal/repository/get_by_id.go
@@ -2,16 +2,31 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/prajkin/em-test-task/internal/domain"
 )
 
 func (r *SubscriptionsRepository) GetByID(ctx context.Context, id uint) (domain.Subscription, error) {
-	var sub domain.Subscription
+	var (
+		sub     domain.Subscription
+		endDate sql.NullTime
+	)
 	err := r.db.QueryRowContext(
 		ctx,
 		"SELECT * FROM subscriptions WHERE id = $1",
 		id,
-	).Scan(&sub.ID, &sub.Name, &sub.Price, &sub.UserID, &sub.StartDate, &sub.EndDate)
-	return sub, err
+	).Scan(&sub.ID, &sub.Name, &sub.Price, &sub.UserID, &sub.StartDate, &endDate)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return domain.Subscription{}, domain.ErrSubscriptionNotFound
+		}
+		return domain.Subscription{}, err
+	}
+
+	if endDate.Valid {
+		sub.EndDate = &endDate.Time
+	}
+
+	return sub, nil
 }
--- internal/repository/list.go
+++ internal/repository/list.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/prajkin/em-test-task/internal/domain"
 )
@@ -18,10 +19,17 @@ func (r *SubscriptionsRepository) List(ctx context.Context) ([]domain.Subscripti
 
 	var subs []domain.Subscription
 	for rows.Next() {
-		var sub domain.Subscription
-		err := rows.Scan(&sub.ID, &sub.Name, &sub.Price, &sub.UserID, &sub.StartDate, &sub.EndDate)
+		var (
+			sub     domain.Subscription
+			endDate sql.NullTime
+		)
+		err := rows.Scan(&sub.ID, &sub.Name, &sub.Price, &sub.UserID, &sub.StartDate, &endDate)
 		if err != nil {
 			return nil, err
+		}
+
+		if endDate.Valid {
+			sub.EndDate = &endDate.Time
 		}
 		subs = append(subs, sub)
 	}
