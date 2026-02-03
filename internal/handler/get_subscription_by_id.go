da097449d40e064c806290c3d491576fc14ffbb2
programmer8760 <programmer8760@gmail.com>
2026-02-03 03:25:55 +0300

    chore: add GetSubscriptionByID http handler

---
+++ internal/handler/get_subscription_by_id.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/prajkin/em-test-task/internal/domain"
+)
+
+func (h *Handler) GetSubscriptionByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id64, err := strconv.ParseUint(r.PathValue("id"), 10, 64)
+	ctx := r.Context()
+
+	resp, err := h.subscriptions.GetSubscriptionByID(ctx, uint(id64))
+	if err == domain.ErrSubscriptionNotFound {
+		http.Error(w, err.Error(), 404)
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	json.NewEncoder(w).Encode(resp)
+}
