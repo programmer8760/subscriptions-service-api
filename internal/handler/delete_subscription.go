248f5f5186933756c9cee45f4263ddcc4cce7846
programmer8760 <programmer8760@gmail.com>
2026-02-04 01:47:35 +0300

    chore: add DeleteSubscription http handler

---
+++ internal/handler/delete_subscription.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/prajkin/em-test-task/internal/domain"
+)
+
+func (h *Handler) DeleteSubscription(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id64, err := strconv.ParseUint(r.PathValue("id"), 10, 64)
+	ctx := r.Context()
+
+	err = h.subscriptions.DeleteSubscription(ctx, uint(id64))
+	if err == domain.ErrSubscriptionNotFound {
+		http.Error(w, err.Error(), 404)
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
