0cbf2bd70ca1b5032a430f22c28d54991eba4961
programmer8760 <programmer8760@gmail.com>
2026-02-03 03:16:00 +0300

    chore: add ErrInvalidID and ErrSubscriptionNotFound

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,7 +3,9 @@ package domain
 import "errors"
 
 var (
-	ErrInvalidName      = errors.New("invalid subscription name")
-	ErrInvalidPrice     = errors.New("invalid subscription price")
-	ErrInvalidStartDate = errors.New("invalid subscription start date")
+	ErrInvalidID            = errors.New("invalid subscription id")
+	ErrInvalidName          = errors.New("invalid subscription name")
+	ErrInvalidPrice         = errors.New("invalid subscription price")
+	ErrInvalidStartDate     = errors.New("invalid subscription start date")
+	ErrSubscriptionNotFound = errors.New("subscription not found")
 )
