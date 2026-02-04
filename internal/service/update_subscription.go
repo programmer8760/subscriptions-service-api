b05fe3539dbaa172c828b4db3146cec7633f3c31
programmer8760 <programmer8760@gmail.com>
2026-02-04 03:15:07 +0300

    chore: add UpdateSubscription method for SubscriptionsService

---
+++ internal/service/update_subscription.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"context"
+	"strings"
+
+	"github.com/prajkin/em-test-task/internal/domain"
+	"github.com/prajkin/em-test-task/internal/dto"
+)
+
+func (s *SubscriptionsService) UpdateSubscription(ctx context.Context, req dto.UpdateSubscriptionDTO) error {
+	if req.ID == 0 {
+		return domain.ErrInvalidID
+	}
+	if req.Name == nil && req.Price == nil && req.UserID == nil && req.StartDate == nil && req.EndDate == nil {
+		return domain.ErrNoChanges
+	}
+	if req.Name != nil {
+		if *req.Name = strings.TrimSpace(*req.Name); *req.Name == "" {
+			return domain.ErrInvalidName
+		}
+	}
+	if req.Price != nil {
+		if *req.Price <= 0 {
+			return domain.ErrInvalidPrice
+		}
+	}
+	if req.StartDate != nil {
+		if (*req.StartDate).IsZero() {
+			return domain.ErrInvalidStartDate
+		}
+	}
+	if req.EndDate != nil {
+		if (*req.EndDate).IsZero() {
+			return domain.ErrInvalidEndDate
+		}
+	}
+
+	if err := s.repo.Update(ctx, req); err != nil {
+		return err
+	}
+
+	return nil
+}
