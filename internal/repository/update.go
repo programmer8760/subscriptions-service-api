60d70115e1307bca2c87919f1350d8ce7a3026c3
programmer8760 <programmer8760@gmail.com>
2026-02-06 02:47:34 +0300

    fix: change the way Update query is built

--- internal/repository/update.go
+++ internal/repository/update.go
@@ -2,56 +2,27 @@ package repository
 
 import (
 	"context"
-	"strconv"
-	"strings"
 
 	"github.com/prajkin/em-test-task/internal/domain"
 	"github.com/prajkin/em-test-task/internal/dto"
 )
 
 func (r *SubscriptionsRepository) Update(ctx context.Context, req dto.UpdateSubscriptionDTO) error {
-	var builder strings.Builder
-	var args []any
-
-	builder.WriteString("UPDATE subscriptions SET ")
-	if req.Name != nil {
-		builder.WriteString("name = $")
-		builder.WriteString(strconv.Itoa(len(args) + 1))
-		builder.WriteRune(' ')
-		args = append(args, req.Name)
-	}
-	if req.Price != nil {
-		builder.WriteString("price = $")
-		builder.WriteString(strconv.Itoa(len(args) + 1))
-		builder.WriteRune(' ')
-		args = append(args, req.Price)
-	}
-	if req.UserID != nil {
-		builder.WriteString("user_id = $")
-		builder.WriteString(strconv.Itoa(len(args) + 1))
-		builder.WriteRune(' ')
-		args = append(args, req.UserID)
-	}
-	if req.StartDate != nil {
-		builder.WriteString("start_date = $")
-		builder.WriteString(strconv.Itoa(len(args) + 1))
-		builder.WriteRune(' ')
-		args = append(args, req.StartDate.Time)
-	}
-	if req.EndDate != nil {
-		builder.WriteString("end_date = $")
-		builder.WriteString(strconv.Itoa(len(args) + 1))
-		builder.WriteRune(' ')
-		args = append(args, req.EndDate.Time)
-	}
-	builder.WriteString("WHERE id = $")
-	builder.WriteString(strconv.Itoa(len(args) + 1))
-	args = append(args, req.ID)
-
 	res, err := r.db.ExecContext(
 		ctx,
-		builder.String(),
-		args...,
+		`UPDATE subscriptions SET
+		name=COALESCE($1, name),
+		price=COALESCE($2, price),
+		user_id=COALESCE($3, user_id),
+		start_date=COALESCE($4, start_date),
+		end_date=COALESCE($5, end_date)
+		WHERE id=$6`,
+		req.Name,
+		req.Price,
+		req.UserID,
+		req.StartDate,
+		req.EndDate,
+		req.ID,
 	)
 	if err != nil {
 		return err
