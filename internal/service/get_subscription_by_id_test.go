714af4b8126919f9bd211ddf65783c59afae48ec
programmer8760 <programmer8760@gmail.com>
2026-02-13 16:59:57 +0300

    test: add tests for service's GetSubscriptionByID

---
+++ internal/service/get_subscription_by_id_test.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"testing"
+
+	"github.com/programmer8760/subscriptions-service-api/internal/domain"
+)
+
+func TestServiceGetSubscriptionByID(t *testing.T) {
+	tests := []struct {
+		id      uint
+		wantErr error
+	}{
+		{0, domain.ErrInvalidID},
+		{10, nil},
+	}
+	repo := &mockRepository{}
+	log := slog.New(slog.NewTextHandler(io.Discard, nil))
+	svc := NewSubscriptionsService(repo, log)
+
+	for _, tt := range tests {
+		name := fmt.Sprintf("id %d", tt.id)
+		t.Run(name, func(t *testing.T) {
+			_, err := svc.GetSubscriptionByID(context.Background(), tt.id)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("err = %v, want %v", err, tt.wantErr)
+			}
+		})
+	}
+}
